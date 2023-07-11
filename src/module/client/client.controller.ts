d49e1bd478eead0a7cc43342b1d8a4c3a5bf24d3
Peeter69 <dv237968@gmail.com>
2023-07-11 15:19:05 -0500

    add management exception

--- src/module/client/client.controller.ts
+++ src/module/client/client.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   ConflictException,
   NotFoundException,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientDto } from 'src/module/client/dto/Client.dto';
@@ -16,47 +18,38 @@ import { ClientDto } from 'src/module/client/dto/Client.dto';
 export class ClientController {
   constructor(private clienteService: ClientService) {}
   @Get()
-  findClients() {
-    return this.clienteService.findClients();
+  async findClients() {
+    return await this.clienteService.findClients();
+  }
+
+  @Get('email')
+  async getClientByEmail(@Body('email') email: string) {
+    console.log(email);
+    const client = await this.clienteService.findClientByEmail(email);
+    return { data: client, message: 'Se ha encontrado una coincidencia' };
   }
 
   @Get('/:id')
-  async findClient(@Param('id') id: number) {
+  async getClientById(@Param('id') id: number) {
     const client = await this.clienteService.findClientsById(id);
-    if (!client) {
-      throw new NotFoundException('El cliente no existe');
-    }
-    return { data: client };
+    return { data: client, message: 'Se ha encontrado una coincidencia' };
   }
 
   @Post()
   async createClient(@Body() client: ClientDto) {
-    try {
-      const data = await this.clienteService.createClient(client);
-      return { data, message: 'Se creo el nuevo cliente' };
-    } catch (error) {
-      if (error.errno == 1062)
-        throw new ConflictException(
-          `Ese correo ya esta registrado con un Paciente`,
-        );
-    }
+    const data = await this.clienteService.createClient(client);
+    return { data, message: 'Se creo el nuevo cliente' };
   }
 
   @Put()
   async updateClient(@Body() client: ClientDto) {
-    try {
-      return await this.clienteService.updateClient(client);
-    } catch (error) {
-      return { error, message: error.message };
-    }
+    const response = await this.clienteService.updateClient(client);
+    return { response, message: 'El paciente fue actualizado' };
   }
 
   @Delete('/:id')
-  deleteClient(@Param('id') id: number) {
-    try {
-      return this.clienteService.deleteClient(id);
-    } catch (error) {
-      return { error, message: error.message };
-    }
+  async deleteClient(@Param('id') id: number) {
+    const response = await this.clienteService.deleteClient(id);
+    return { response, message: 'El paciente fue eliminado ' };
   }
 }
